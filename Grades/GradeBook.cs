20dcc3741a2b3e6da43267f2691b01af5be251c3
Katherine Hambley <superdesigngirl@outlook.com>
2018-10-31 06:45:24 -0500

    Created Gradebook class, created constructor which initializes grades List for new objects.

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,11 +8,17 @@ namespace Grades
 {
     class GradeBook
     {
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
+        //NullReferenceException, using an uninitialized variable, a variable that points to nothing
         List<float> grades;
 
     }
